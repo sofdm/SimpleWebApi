eeb5f557c5550bd32d57bbd48c02993844462870
Sofdm <sof.dm@hotmail.com>
2019-11-19 16:18:51 +0200

    upload the code

---
+++ WebApplication3/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        static List<string> StringsList = new List<string> ()
+        { 
+            "Luffy", "Sanji", "Zoro" 
+        };
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return StringsList;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+
+            return StringsList[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            StringsList.Add(value);
+            
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            StringsList[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+
+            StringsList.RemoveAt(id);
+        }
+    }
+}
